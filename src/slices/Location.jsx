6b49c1261daf9fe8f8562d4f98b030e13e6c6ef4
Ahmed Ghazi <ahmed.ghazi@soixanteseize.com>
2019-08-20 15:40:59 +0200

    map

--- src/slices/Location.jsx
+++ src/slices/Location.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-// import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet-universal";
 
 class Location extends Component {
   constructor() {
