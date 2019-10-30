9cec31f2bfc48d9e3f8ab1b0818c22b406d71de0
Ahmed Ghazi <ahmed.ghazi@soixanteseize.com>
2019-10-30 15:27:55 +0100

    images margin

--- src/slices/TexteImage.jsx
+++ src/slices/TexteImage.jsx
@@ -5,7 +5,7 @@ const TexteImage = ({ input }) => {
   return (
     <section className="slice texte-image">
       <h2 className="headline b-b pad">{input.primary.title1.text}</h2>
-      <div className="content pad">
+      <div className="content ">
         <div className="row">
           <div className="col-xs-12 col-md-6">
             <div
