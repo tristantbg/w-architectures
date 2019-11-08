6dfce7bd4684c31091408646e2d9cdc818c07783
Ahmed Ghazi <ahmed.ghazi@soixanteseize.com>
2019-11-08 15:57:31 +0100

    header curseur

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -8,3 +8,21 @@ const NotFound = () => (
 );
 
 export default NotFound;
+
+// import React from "react";
+// import SEO from "../components/seo";
+
+// const browser = typeof window !== "undefined" && window;
+
+// const NotFoundPage = () => {
+//   return (
+//     browser && (
+//       <div>
+//         <SEO title="404 Page not found" />
+//         <h1>404 Error content...</h1>
+//       </div>
+//     )
+//   );
+// };
+
+// export default NotFoundPage;
